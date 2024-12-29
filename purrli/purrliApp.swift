835bece3a81fe88898947e5042488e15b8e7f8da
Mac Mini M4 <jinwenliu@mac.lan>
2024-12-29 15:02:23 -0500

    initial creation by Xcode

---
+++ purrli/purrliApp.swift
@@ -0,0 +1,17 @@
+//
+//  purrliApp.swift
+//  purrli
+//
+//  Created by Jinwen Liu on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct purrliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ purrliTests/purrliTests.swift
@@ -0,0 +1,17 @@
+//
+//  purrliTests.swift
+//  purrliTests
+//
+//  Created by Jinwen Liu on 12/29/24.
+//
+
+import Testing
+@testable import purrli
+
+struct purrliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
